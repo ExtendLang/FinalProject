381023a1aa256913246567eceaaa2022f8c20553
Nigel Schuster <nigel.schusters@googlemail.com>
2016-12-19 22:35:25 -0500

    Updated submodule to latest

--- generate_gitlogs.sh
+++ generate_gitlogs.sh
@@ -4,12 +4,12 @@ TEX_FILE=tex/git.tex
 rm $TEX_FILE
 touch $TEX_FILE
 
-echo "\\medskip \\noindent" >> $TEX_FILE
+echo "\\\\medskip \\\\noindent" >> $TEX_FILE
 
-echo "\\chapter{Git Logs}" >> $TEX_FILE
-echo "\\begin{lstlisting}" >> $TEX_FILE
+echo "\\\\chapter{Git Logs}" >> $TEX_FILE
+echo "\\\\begin{lstlisting}" >> $TEX_FILE
 
 git --git-dir=$CODE_LOC/.git log --pretty=format:"%h %cI %cn: %s" >> $TEX_FILE
 
 echo -e "\n" >> $TEX_FILE
-echo "\\end{lstlisting}" >> $TEX_FILE
+echo "\\\\end{lstlisting}" >> $TEX_FILE
